d356c1903fdeea08d94c87da4be2688204367036
xyrlsz <xyrlsz@outlook.com>
2025-03-15 18:31:29 +0800

    upload the first version.

---
+++ config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+
+	_ "embed"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	ListenOn         string   `yaml:"ListenOn"`
+	Port             uint16   `yaml:"Port"`
+	RelativePath     string   `yaml:"RelativePath"`
+	LocalAssetsPaths []string `yaml:"LocalAssetsPaths"`
+	NodeHosts        []string `yaml:"NodeHosts"`
+}
+
+//go:embed config.yaml
+var configData []byte
+
+var (
+	confInstance *Config
+	confOnce     sync.Once
+)
+
+func initConfig() {
+	confOnce.Do(func() {
+		configPath := filepath.Join("etc", "config.yaml")
+		if _, err := os.Stat(configPath); os.IsNotExist(err) {
+			// 创建目录并写入默认配置文件
+			if err := os.MkdirAll("etc", 0755); err != nil {
+				log.Default().Panicln("Failed to create etc directory:", err)
+			}
+			if err := os.WriteFile(configPath, configData, 0644); err != nil {
+				log.Default().Panicln("Failed to write config file:", err)
+			}
+			log.Default().Println("Please edit etc/config.yaml.")
+			os.Exit(0)
+		} else if err != nil {
+			log.Default().Panicln("Failed to check config file:", err)
+		} else {
+
+			data, err := os.ReadFile(configPath)
+			if err != nil {
+				log.Default().Panicln("Failed to read config file:", err)
+			}
+			confInstance = &Config{}
+			if err := yaml.Unmarshal(data, confInstance); err != nil {
+				log.Default().Panicln("Failed to parse config file:", err)
+			}
+		}
+	})
+}
+
+func GetConfig() *Config {
+	initConfig()
+	return confInstance
+}
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"path/filepath"
+	"simcdn/config"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	nodes     = []string{} // 其他节点地址
+	nodeIndex = 0
+	nodeMutex = &sync.Mutex{}
+)
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	conf := config.GetConfig()
+	nodes = conf.NodeHosts
+
+	// 处理动态资源请求
+	r.GET(fmt.Sprintf("/%s/*filename", conf.RelativePath), func(c *gin.Context) {
+		filename := c.Param("filename")[1:] // 去掉前导 "/"
+
+		// 检查本地文件
+		for _, staticDir := range conf.LocalAssetsPaths {
+			filePath := filepath.Join(staticDir, filename)
+			if _, err := os.Stat(filePath); err == nil {
+				c.File(filePath)
+				return
+			}
+
+		}
+
+		// 若 nodes 为空，则返回 404
+		if len(nodes) == 0 {
+			c.String(http.StatusNotFound, "File not found")
+			return
+		}
+
+		// 从其他节点获取资源
+		nodeMutex.Lock()
+		nodeURL := nodes[nodeIndex]
+		nodeIndex = (nodeIndex + 1) % len(nodes) // 轮询选择节点
+		nodeMutex.Unlock()
+
+		target, err := url.Parse(nodeURL)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Invalid node URL")
+			return
+		}
+
+		// 反向代理到其他 CDN 节点
+		proxy := httputil.NewSingleHostReverseProxy(target)
+		proxy.ServeHTTP(c.Writer, c.Request)
+	})
+
+	// 启动服务器
+	fmt.Printf("CDN server has started on  %s:%d.\n", conf.ListenOn, conf.Port)
+	if err := r.Run(fmt.Sprintf("%s:%d", conf.ListenOn, conf.Port)); err != nil {
+		log.Default().Fatal(fmt.Sprint("Server failed to start:", err))
+	}
+}
